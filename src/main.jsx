b1b1018d25f46096df7c7c54088dd121d9ea17ef
Florent HORTALA <p.ehb12@gmail.com>
2026-05-16 17:12:30 +0200

    1

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './Css/index.css'
+import './css/index.css'
 import LayoutSelector from './component/lib/layoutSelector/LayoutSelector';
 
 createRoot(document.getElementById('root')).render(
